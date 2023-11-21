566f1b0efe21eb5ea5ad4904d3dcca200a220a7f
Suwon Jeong <integer.won@gmail.com>
2023-11-22 12:47:35 +1300

    fifififinal

--- client/apis/recipes.ts
+++ client/apis/recipes.ts
@@ -1,6 +1,5 @@
 import request from 'superagent'
 import { Recipes } from '../../models/recipes'
-import { User } from '../../models/users'
 
 const rootUrl = '/api/v1'
 
@@ -8,18 +7,3 @@ export async function getRecipes() {
   const res = await request.get(rootUrl + `/recipes`)
   return res.body.recipes as Recipes[]
 }
-
-// export async function getAllSavedRecipes(): Promise<SavedRecipes[]> {
-//   const res = await request.get(rootUrl + `/recipes/saved-recipes/`)
-//   return res.body
-// }
-
-// export async function getSavedRecipes({
-//   user_id,
-// }: {
-//   user_id: number
-// }): Promise<SavedRecipes[]> {
-//   const res = await request.get(rootUrl + `/recipes/saved-recipes/${user_id}`)
-
-//   return res.body
-// }
