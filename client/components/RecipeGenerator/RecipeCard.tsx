8e04a8661701c368b14599a5ac97af312dfabe05
Suwon Jeong <integer.won@gmail.com>
2023-11-23 14:39:39 +1300

    add function for saving recipes

--- client/components/RecipeGenerator/RecipeCard.tsx
+++ client/components/RecipeGenerator/RecipeCard.tsx
@@ -6,34 +6,72 @@ import RecipeViewer from '../ViewRecipe/RecipeViewer'
 import SideBar from '../SideBar/SideBar'
 import Header from '../Header/Header'
 import { useRecipe } from '../../hooks/useRecipe.ts'
+import styles from './RecipeGenerator.module.css'
+import { Recipes } from '../../../models/recipes.ts'
+import { useQuery } from '@tanstack/react-query'
+import { getRecipes } from '../../apis/recipes.ts'
 
 const RecipeCard = () => {
   const { user } = useAuth0()
   const { state } = useLocation()
   const navigate = useNavigate()
-
-  const auth0Id = user?.sub
-
-  
-
   const { addRecipeMutation } = useRecipe()
 
-  // const handleSave = (id: number) => {
-  //   addRecipeMutation.mutate()
-  //   navigate('/')
-  //   window.location.reload()
-  // }
+  const auth0Id = user?.sub
+  console.log('state' + state)
+  const {
+    data: allRecipes,
+    isError,
+    isLoading,
+  } = useQuery(['allRecipes'], getRecipes)
 
-  
+  if (isError) {
+    return (
+      <>
+        <p>Something went wrong!</p>
+      </>
+    )
+  }
+
+  if (!allRecipes || isLoading) {
+    return <p>...loading</p>
+  }
+  const lastRecipeId = allRecipes[allRecipes.length - 1]?.id || 0
+  const newId = lastRecipeId + 1
+
+  const newRecipe: Recipes = {
+    id: newId,
+    dish_name: state.dish_name,
+    preparation_time: state.preparation_time,
+    cooking_time: state.cooking_time,
+    servings: state.servings,
+    ingredients: state.ingredients,
+    method: state.method,
+    auth0_id: auth0Id || '',
+  }
+  console.log('newRecipe' + newRecipe)
+
+  const handleSave = () => {
+    addRecipeMutation.mutate(newRecipe, {
+      onSuccess: () => {
+        navigate('/')
+        window.location.reload()
+      },
+    })
+  }
+
   return (
     <div>
       <Header />
       <SideBar />
       <h1>Recipe for {user?.nickname}</h1>
       <IfAuthenticated>
-      {/* <div className={styles['button-container']}>
-        <button onClick={() => handleSave(state.id)}>Save</button>
-      </div> */}
+          <button
+            className={styles['button']}
+            onClick={() => handleSave()}
+          >
+            Save
+          </button>
       </IfAuthenticated>
       <IfNotAuthenticated>
         <p>You can save this recipe after login </p>
