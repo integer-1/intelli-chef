15c5158ce7b0e003c72a4663382e9ca79aeabc32
Benedict Velasco <benedict.velasco@outlook.com>
2023-11-27 19:08:51 +1300

    can now detect if ingredients list is empty and will prompt user

--- client/components/RecipeGenerator/RecipeGenerator.tsx
+++ client/components/RecipeGenerator/RecipeGenerator.tsx
@@ -9,7 +9,7 @@ import ErrorMessage from '../Error/ErrorMessage'
 function RecipeGenerator() {
   //state of recipeList is passed to/from RecipeCard to provide go-back functionality
   const { state } = useLocation()
-  const [recipeList, setRecipeList] = useState<Recipes[]>()
+  const [recipeList, setRecipeList] = useState<Recipes[] | null>(null)
 
   //set initial recipeList if returning from recipe view
   // useEffect(() => {
@@ -23,6 +23,8 @@ function RecipeGenerator() {
     isLoading,
     isError,
   } = useQuery(['ingredients'], getAllIngredients)
+
+  let ingredientsList: string
 
   const fetchData = async () => {
     try {
@@ -39,50 +41,59 @@ function RecipeGenerator() {
         )
       }
 
-      const ingredientsList = ingredients
-        .map((ingredient) => ingredient.item_name)
-        .join(', ')
-      const prompt = `From now you when you respond you will only provide a codeblock with json and nothing else. You will consider this list of ingredients: ${ingredientsList} and provide a maximum of 3 recipes containing ONLY the ingredients specific and absolutely no additional ingredients. The json will have the following properties: dish_name, preparation_time, cooking_time, servings, ingredients and method. Please store each step of method as a string array. Remember you must provide only a codeblock containing json, absolutely no additional text. If ${ingredientsList} is empty, return this message "Please provide ingredients".`
+      if (ingredients && ingredients.length > 0) {
+        console.log('ingredients detected')
+        ingredientsList = ingredients
+          ?.map((ingredient) => ingredient.item_name)
+          .join(', ')
 
-      const options = {
-        method: 'POST',
-        body: JSON.stringify({
-          message: prompt,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
+        const prompt = `From now you when you respond you will only provide a codeblock with json and nothing else. You will consider this list of ingredients: ${ingredientsList} and provide a maximum of 3 recipes containing ONLY the ingredients specific and absolutely no additional ingredients. The json will have the following properties: dish_name, preparation_time, cooking_time, servings, ingredients and method. Please store each step of method as a string array. Remember you must provide only a codeblock containing json, absolutely no additional text. If ${ingredientsList} is empty, return this message "Please provide ingredients".`
 
-      const response = await fetch('http://localhost:3000/completions', options)
-
-      if (!response.ok) {
-        throw new Error(`Server responded with status: ${response.status}`)
-      }
-
-      const data = await response.json()
-
-      if (data.choices && data.choices.length > 0) {
-        //console.log(data.choices[0].message['content'])
-        const stringValue1: string = data.choices[0].message['content']
-        const refinedData = stringValue1.replace(/\\|\n|`|json|/g, '')
-        const jsonArray = JSON.parse(refinedData)
-        for (let i = 0; i < jsonArray.length; i++) {
-          jsonArray[i].ingredients = Array.isArray(jsonArray[i].ingredients)
-            ? jsonArray[i].ingredients.join(', ')
-            : String(jsonArray[i].ingredients)
-          jsonArray[i].method = Array.isArray(jsonArray[i].method)
-            ? jsonArray[i].method.join('\n')
-            : String(jsonArray[i].method)
+        const options = {
+          method: 'POST',
+          body: JSON.stringify({
+            message: prompt,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
         }
-        setRecipeList(jsonArray)
-      } else {
-        const message = `'No choices found in the response.`
-        return (
-          <>
-            <ErrorMessage message={message} />
-          </>
+
+        const response = await fetch(
+          'http://localhost:3000/completions',
+          options
         )
+
+        if (!response.ok) {
+          throw new Error(`Server responded with status: ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        if (data.choices && data.choices.length > 0) {
+          //console.log(data.choices[0].message['content'])
+          const stringValue1: string = data.choices[0].message['content']
+          const refinedData = stringValue1.replace(/\\|\n|`|json|/g, '')
+          const jsonArray = JSON.parse(refinedData)
+          for (let i = 0; i < jsonArray.length; i++) {
+            jsonArray[i].ingredients = Array.isArray(jsonArray[i].ingredients)
+              ? jsonArray[i].ingredients.join(', ')
+              : String(jsonArray[i].ingredients)
+            jsonArray[i].method = Array.isArray(jsonArray[i].method)
+              ? jsonArray[i].method.join('\n')
+              : String(jsonArray[i].method)
+          }
+          setRecipeList(jsonArray)
+        } else {
+          const message = `'No choices found in the response.`
+          return (
+            <>
+              <ErrorMessage message={message} />
+            </>
+          )
+        }
+      } else {
+        console.log('No ingredients!')
       }
     } catch (error) {
       const message = `Currently, our chef is on sick leave. Sorry.`
@@ -105,8 +116,10 @@ function RecipeGenerator() {
       <div>
         <h3>Chat GPT : </h3>
         <ul>
-          {recipeList === null ? (
-            <p>Loading...</p>
+          {!ingredients || ingredients.length === 0 ? (
+            <p>
+              No ingredients selected. Please add at least one item to continue.
+            </p>
           ) : recipeList?.length ? (
             recipeList.map((recipe) => (
               <li key={recipe.dish_name}>
